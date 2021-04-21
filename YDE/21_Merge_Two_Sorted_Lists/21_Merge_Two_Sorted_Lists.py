7840aa1069765561186fdb9f4ef2f740151587d4
YunPC <apsn2000@nate.com>
2021-04-21 19:54:42 +0900

    feat: Solved 21_Merge_Two_Sorted_Lists.py with Recursion

--- YDE/21_Merge_Two_Sorted_Lists/21_Merge_Two_Sorted_Lists.py
+++ YDE/21_Merge_Two_Sorted_Lists/21_Merge_Two_Sorted_Lists.py
@@ -89,3 +89,13 @@ def solution2(l1, l2):
                     
     return list_node
 
+# Runtime: 56 ms, Memory Usage: 14.4 MB
+def solution3(l1, l2):
+
+    if (not l1) or (l2 and l1.val > l2.val):
+            l1, l2 = l2, l1
+            
+    if l1:
+        l1.next = self.mergeTwoLists(l1.next, l2)
+        
+    return l1
