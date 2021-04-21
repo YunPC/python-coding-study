8ec1dc0e14b584100db68769e5c7c25868f9e8c7
YunPC <46584354+YunPC@users.noreply.github.com>
2021-04-21 21:05:45 +0900

    feat: Solved Linked list, stack, queue prob. in leetcode(YDE #26)
    * refactor: Make personal folder in YDE
    
    * feat: Solved 21_Merge_Two_Sorted_Lists with sorting
    
    * feat: Solved 21_Merge_Two_Sorted_Lists with merging
    
    * feat: Solved 21_Merge_Two_Sorted_Lists.py with Recursion
    
    * docs: Description of 21_Merge_Two_Sorted_Lists
    
    * feat: Solved 206_Reverse_Linked_List with iterate
    
    * feat:Solved 206_Reverse_Linked_List with recursion
    
    * docs: Description of 206_Reverse_Linked_List
    
    * feat: Initiate circular queue class
    
    * feat: Add Enqueue function
    
    enqueue value in circular queue if queue is not full
    
    * feat: Add deQueue in circular queue
    
    return value if queue is not empty
    
    * feat: Add Front function in circular queue
    
    Return oldest value in queue
    
    * feat: Add Rear function in circuar queue
    
    Return last value in queue
    
    * feat: Add isEmpty function in circular queue
    
    Check if queue is empty
    
    * feat: Add isFull function in circular queue
    
    Check if the circular queue is full
    
    * perf:Delete if condition in Dequeue
    
    Dequeue function always remvoe element in queue so full variable always be false
    
    * docs:Description of Design Circular Queue
    
    * docs:Write README.md(Linked-List, Queue)

---
+++ YDE/21_Merge_Two_Sorted_Lists/21_Merge_Two_Sorted_Lists.py
@@ -0,0 +1,101 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+# Runtime: 40 ms, Memory Usage: 14.4 MB
+def solution1(l1, l2):
+    nodes = []
+        
+    node = l1
+    
+    while node is not None:
+        nodes.append(node.val)
+        node = node.next
+        
+    node = l2
+    
+    while node is not None:
+        nodes.append(node.val)
+        node = node.next
+        
+    if len(nodes) == 0:
+        return ListNode('')
+    
+    nodes.sort()
+    
+    list_node = ListNode(nodes[0])
+    
+    head = list_node
+    
+    for i in range(1, len(nodes)):
+        head.next = ListNode(nodes[i])
+        head = head.next
+        
+    return list_node
+
+# Runtime: 60 ms, Memory Usage: 14.4 MB
+def solution2(l1, l2):
+
+    if l1.val == '' and l2.val == '':
+            return ListNode('')
+    
+    list_node = ListNode('')
+
+    head = list_node
+
+    while l1 or l2:
+        
+        if l1 is None:
+            
+            if head.val == '':
+                head.val = l2.val
+            else:
+                head.next = ListNode(l2.val)
+                head = head.next
+            
+            l2 = l2.next
+            
+        elif l2 is None:
+            
+            if head.val == '':
+                head.val = l1.val
+            else:
+                head.next = ListNode(l1.val)
+                head = head.next
+            
+            l1 = l1.next
+            
+        else:
+            if l1.val <= l2.val:
+                
+                if head.val == '':
+                    head.val = l1.val
+                else:
+                    head.next = ListNode(l1.val)
+                    head = head.next
+                    
+                l1 = l1.next
+                
+            else:
+                if head.val == '':
+                    head.val = l2.val
+                else:
+                    head.next = ListNode(l2.val)
+                    head = head.next
+                    
+                l2 = l2.next
+                    
+    return list_node
+
+# Runtime: 56 ms, Memory Usage: 14.4 MB
+def solution3(l1, l2):
+
+    if (not l1) or (l2 and l1.val > l2.val):
+            l1, l2 = l2, l1
+            
+    if l1:
+        l1.next = self.mergeTwoLists(l1.next, l2)
+        
+    return l1
