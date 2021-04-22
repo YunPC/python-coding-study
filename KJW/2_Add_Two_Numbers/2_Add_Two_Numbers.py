17fa29804a5d7b9b2f2610d8972eb183ba906556
kimjeongwonn <66168187+kimjeongwonn@users.noreply.github.com>
2021-04-22 11:09:27 +0900

    docs: Add new solution of 2_Add_Two_Numbers(KJW #29)
    * feat : Two subjects resolved
    
    1. isPalindrome
    2. reverseString
    
    * feat: 561_array_partition resolved!
    
    * fix:
    
    * fix: gitignore recovery..
    
    * fix: gitignore recovery..
    
    * feat: resolved assignment no.2
    
    * feat: resolve 3 assignments, improve directory architeture
    
    1. 2_Add_Two_Numbers
    2. 20_Valid_Parentheses
    3. 225_Implement_Stack_Using_Queue
    
    improve directory architecture.
    /{NAME_INITIAL}/{ASSIGNMENT_NAME}
    
    * docs: Add two numbers docs improve
    
    * refactor: Add new solution with adder

--- KJW/2_Add_Two_Numbers/2_Add_Two_Numbers.py
+++ KJW/2_Add_Two_Numbers/2_Add_Two_Numbers.py
@@ -38,14 +38,14 @@ class Solution:
         l2_num = 0
         i = 1
         while l1:
-            l1_num += l1.val * (10 ** i)//10
+            l1_num += l1.val * i
             l1 = l1.next
-            i += 1
+            i *= 10
         i = 1
         while l2:
-            l2_num += l2.val * (10 ** i)//10
+            l2_num += l2.val * i
             l2 = l2.next
-            i += 1
+            i *= 10
         l_sum = l1_num+l2_num
         l_sum, l_mod = divmod(l_sum, 10)
         s_node = ListNode(l_mod)
@@ -55,3 +55,32 @@ class Solution:
             target.next = ListNode(l_mod)
             target = target.next
         return s_node
+
+# 교과서 풀이
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        root = head = ListNode(0)
+
+        carry = 0
+        while l1 or l2 or carry:
+            sum = 0
+
+            if l1:
+                sum += l1.val
+                l1 = l1.next
+            if l2:
+                sum += l2.val
+                l2 = l2.next
+
+            carry, val = divmod(sum+carry, 10)
+            head.next = ListNode(val)
+            head = head.next
+        return root.next
